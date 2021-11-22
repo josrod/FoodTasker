8d7d1120906fd0757471e0a539c7203c9a696ee3
Jose Luis Rodriguez <ucjackal@gmail.com>
2021-11-22 19:59:24 +0100

    finish heroku config

--- FoodTasker/settings.py
+++ FoodTasker/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dj_database_url import parse as db_url
 from pathlib import Path
+import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mkr(3f*(&l3x^6(3qvt5&gr7lakbn0+w$jru^n$@r0rfx)r&e6'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
 
 # Application definition
@@ -82,7 +87,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#DATABASE_URL = os.environ.get('DATABASE_URL')
+#db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+#DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -119,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
@@ -129,18 +136,47 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='/'
 
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+
+SOCIAL_AUTH_FACEBOOK_KEY = config("SOCIAL_AUTH_FACEBOOK_KEY")
+SOCIAL_AUTH_FACEBOOK_SECRET = config("SOCIAL_AUTH_FACEBOOK_SECRET")
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'id, name, email'
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'Lingen Food Tasker <no-reply@foodtasker.localhost>'
+
+STRIPE_API_PUBLIC_KEY = config("STRIPE_API_PUBLIC_KEY")
+STRIPE_API_SECRET_KEY = config("STRIPE_API_SECRET_KEY")
+
+GOOGLE_MAP_API_KEY = config("GOOGLE_MAP_API_KEY")
+
+PAYPAL_MODE = "sandbox"
+PAYPAL_CLIENT_ID = config("PAYPAL_CLIENT_ID")
+PAYPAL_CLIENT_SECRET = config("PAYPAL_CLIENT_SECRET")
+
+#NOTIFICATION_URL = "https://infinite-taiga-87568.herokuapp.com/"
+
+#ASGI_APPLICATION = "foodtasker.asgi.application"
+
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
 cloudinary.config(
-    cloud_name="dmqco0jou",
-    api_key="169878439997933",
-    api_secret="V-vWTQJe-AY7C4Mrb6DucUugBkc"
-    
+    cloud_name=config("CLOUDINARY_NAME"), 
+    api_key=config("CLOUDINARY_API_KEY"), 
+    api_secret=config("CLOUDINARY_API_SECRET")
 )
 
 #Configure Django app for Heroku
-import django_heroku
 django_heroku.settings(locals())
 
